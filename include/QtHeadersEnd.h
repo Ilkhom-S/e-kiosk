bd9ca623bb7ddd9a9df27035bb67a09e98d80995
Ilkhom Safarov <safarovi@humo.tj>
2026-01-12 11:59:30 +0500

    chore(include): add Qt header wrappers and wrap common Qt includes

---
+++ include/QtHeadersEnd.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Restore warning state after including Qt headers
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#elif defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
