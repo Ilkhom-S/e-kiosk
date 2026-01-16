de60bb8df3d84fa843f1330cae9d5f6f94cf08fb
Ilkhom Safarov <safarovi@humo.tj>
2026-01-16 11:29:46 +0500

    refactor(range): make src header a thin wrapper including public header to avoid duplicate declarations

--- src/modules/SDK/PaymentProcessor/src/Range.h
+++ src/modules/SDK/PaymentProcessor/src/Range.h
@@ -1,39 +1,8 @@
-/* @file Диапазон номерной ёмкости. */
-
 #pragma once
 
-// Qt
-#include <Common/QtHeadersBegin.h>
-#include <QtCore/QVector>
-#include <Common/QtHeadersEnd.h>
+// Public API for Range is provided in the project include tree. Make this a thin wrapper
+// so sources that include "Range.h" continue to work, but the canonical declaration lives
+// in "include/SDK/PaymentProcessor/Settings/Range.h".
 
-namespace SDK {
-    namespace PaymentProcessor {
-
-        //----------------------------------------------------------------------------
-        /// Диапазон номерной ёмкости.
-        struct SRange {
-            /// Границы диапазона.
-            qint64 from;
-            qint64 to;
-
-            /// cid и id операторов.
-            QVector<qint64> cids;
-            QVector<qint64> ids;
-
-            /// Оператор сравнения для сортировки списка из SRange.
-            bool operator<(const SRange &aRange) const;
-        };
-
-        //---------------------------------------------------------------------------
-        /// Оператор сравнения для поиска диапазона в который входит aNumber c помощью qLowerBound().
-        bool operator<(const SRange &aRange, qint64 aNumber);
-
-        //---------------------------------------------------------------------------
-        /// Оператор сравнения для поиска диапазона в который входит aNumber с помощью qUpperBound().
-        bool operator<(qint64 aNumber, const SRange &aRange);
-
-    } // namespace PaymentProcessor
-} // namespace SDK
-
-//---------------------------------------------------------------------------
+// SDK
+#include <SDK/PaymentProcessor/Settings/Range.h>
