d1d2ad4fa816fa2e49e342974eaa80efb00c88ca
Ilkhom Safarov <safarovi@humo.tj>
2026-01-16 11:27:08 +0500

    fix(include): make public Range.h self-contained (no src-relative includes)

--- include/SDK/PaymentProcessor/Settings/Range.h
+++ include/SDK/PaymentProcessor/Settings/Range.h
@@ -1,2 +1,29 @@
-// System
-#include "../../../../../src/modules/SDK/PaymentProcessor/src/Range.h"
+#pragma once
+
+// Qt
+#include <Common/QtHeadersBegin.h>
+#include <QtCore/QVector>
+#include <Common/QtHeadersEnd.h>
+
+namespace SDK {
+    namespace PaymentProcessor {
+
+        //----------------------------------------------------------------------------
+        /// Range of ids/capacity.
+        struct SRange {
+            qint64 from;
+            qint64 to;
+
+            QVector<qint64> cids;
+            QVector<qint64> ids;
+
+            bool operator<(const SRange &aRange) const;
+        };
+
+        //---------------------------------------------------------------------------
+        bool operator<(const SRange &aRange, qint64 aNumber);
+        bool operator<(qint64 aNumber, const SRange &aRange);
+
+    } // namespace PaymentProcessor
+} // namespace SDK
+
