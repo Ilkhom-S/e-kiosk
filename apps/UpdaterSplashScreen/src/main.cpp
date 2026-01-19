69d5084054122e3b38cc1b8a80f154db80e96aef
Ilkhom Safarov <safarovi@humo.tj>
2026-01-19 18:04:33 +0500

    chore: remove UpdaterSplashScreen .gitkeep and add src/ folder

---
+++ apps/UpdaterSplashScreen/src/main.cpp
@@ -0,0 +1,16 @@
+/* @file Mainline. */
+
+// Qt
+#include <Common/QtHeadersBegin.h>
+#include <QtWidgets/QApplication>
+#include <Common/QtHeadersEnd.h>
+
+// Project
+#include "UpdaterSplashScreen.h"
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    UpdaterSplashScreen w;
+    w.showFullScreen();
+    return a.exec();
+}
