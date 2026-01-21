707e9c643e66e193eaaa17314c236c8c9b73983a
Ilkhom Safarov <ilhomjon.s@gmail.com>
2026-01-21 20:43:32 +0500

    build: finalize .gitignore and translation source tracking; CMake translation workflow now robust

---
+++ apps/WatchServiceController/src/locale/WatchServiceController_tg.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="tg">
+  <context>
+    <name>WatchServiceController</name>
+    <message>
+      <location filename="../WatchServiceController.cpp" line="41" />
+      <source>#start_service_menu</source>
+      <translation>Менюи хизматрасониро оғоз кунед</translation>
+    </message>
+    <message>
+      <location filename="../WatchServiceController.cpp" line="46" />
+      <source>#start_first_setup</source>
+      <translation>Танзимоти ибтидоиро оғоз кунед</translation>
+    </message>
+    <message>
+      <location filename="../WatchServiceController.cpp" line="54" />
+      <source>#start_service</source>
+      <translation>Хизматрасониро оғоз кунед</translation>
+    </message>
+    <message>
+      <location filename="../WatchServiceController.cpp" line="59" />
+      <source>#stop_service</source>
+      <translation>Хизматрасониро қатъ кунед</translation>
+    </message>
+    <message>
+      <location filename="../WatchServiceController.cpp" line="61" />
+      <source>#close</source>
+      <translation>Баромад</translation>
+    </message>
+    <message>
+      <location filename="../WatchServiceController.cpp" line="189" />
+      <source>#exit</source>
+      <translation>Баромад</translation>
+    </message>
+    <message>
+      <location filename="../WatchServiceController.cpp" line="190" />
+      <source>#confirm_close_trayicon</source>
+      <translation>Шумо воқеан мехоҳед идоракунии хидмати назоратиро пӯшед?</translation>
+    </message>
+    <message>
+      <location filename="../WatchServiceController.cpp" line="200" />
+      <source>#yes</source>
+      <translation>Бале</translation>
+    </message>
+    <message>
+      <location filename="../WatchServiceController.cpp" line="201" />
+      <source>#no</source>
+      <translation>Не</translation>
+    </message>
+  </context>
+</TS>
