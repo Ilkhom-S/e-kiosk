f6a0316b92bfd94956ce111a4b524f499324fcdd
Ilkhom Safarov <ilhomjon.s@gmail.com>
2026-01-22 07:55:16 +0500

    docs(common): clarify harmless diagnostic pop warning in QtHeadersEnd.h for Linux/macOS/Clang/GCC

--- include/Common/QtHeadersEnd.h
+++ include/Common/QtHeadersEnd.h
@@ -11,6 +11,16 @@
          #include <Common/QtHeadersEnd.h>
 */
 
+/*
+      Note for Linux/macOS/Clang/GCC users:
+      ------------------------------------------------------------
+      You may see a warning like:
+            'pragma diagnostic pop could not pop, no matching push [-Wunknown-pragmas]'
+      This is harmless and occurs if QtHeadersBegin.h did not push diagnostics, or if the compiler does not support the pragma.
+      It does not affect compilation or runtime. Safe to ignore.
+      On Windows/MSVC, warning suppression works as intended.
+*/
+
 #if defined(_MSC_VER)
 #pragma warning(pop)
 #undef HUMO_SUPPRESS_QT_WARNINGS
