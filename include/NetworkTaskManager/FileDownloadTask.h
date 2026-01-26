79692b7deff7e5f1780a09973796fcea048ea01f
Ilkhom Safarov <ilhomjon.s@gmail.com>
2026-01-26 16:33:57 +0500

    refactor(include): migrate NetworkTaskManager/FileDownloadTask.h to public API structure

--- include/NetworkTaskManager/FileDownloadTask.h
+++ include/NetworkTaskManager/FileDownloadTask.h
@@ -1,4 +1,32 @@
-/* @file Задача загрузки файла. */
+/* @file Реализация класса-запроса для скачивания файла с докачкой. */
 
-// System
-#include "../../modules/NetworkTaskManager/src/FileDownloadTask.h"
+#pragma once
+
+#include <Common/QtHeadersBegin.h>
+#include <QtCore/QUrl>
+#include <QtCore/QString>
+#include <Common/QtHeadersEnd.h>
+
+#include "NetworkTask.h"
+
+//------------------------------------------------------------------------
+class FileDownloadTask : public NetworkTask {
+    Q_OBJECT
+
+  public:
+    FileDownloadTask(const QUrl &aUrl, const QString &aPath);
+
+    QString getPath() const;
+
+    void closeFile();
+
+  public slots:
+    /// Удаляем файл и создаем его заново
+    void resetFile();
+
+  protected:
+    QUrl mUrl;
+    QString mPath;
+};
+
+//------------------------------------------------------------------------
--- src/modules/NetworkTaskManager/src/FileDownloadTask.h
+++ src/modules/NetworkTaskManager/src/FileDownloadTask.h
@@ -1,32 +1,9 @@
-/* @file Реализация класса-запроса для скачивания файла с докачкой. */
+/* @file DEPRECATED - See ../../../<NetworkTaskManager/FileDownloadTask.h> instead.
+
+MIGRATION NOTE: This file kept for backward compatibility only.
+The class definition has been moved to the public header in ../../../<NetworkTaskManager/FileDownloadTask.h>.
+All NEW code should include ../../../<NetworkTaskManager/FileDownloadTask.h>.
+*/
 
 #pragma once
-
-#include <Common/QtHeadersBegin.h>
-#include <QtCore/QUrl>
-#include <QtCore/QString>
-#include <Common/QtHeadersEnd.h>
-
-#include "NetworkTask.h"
-
-//------------------------------------------------------------------------
-class FileDownloadTask : public NetworkTask {
-    Q_OBJECT
-
-  public:
-    FileDownloadTask(const QUrl &aUrl, const QString &aPath);
-
-    QString getPath() const;
-
-    void closeFile();
-
-  public slots:
-    /// Удаляем файл и создаем его заново
-    void resetFile();
-
-  protected:
-    QUrl mUrl;
-    QString mPath;
-};
-
-//------------------------------------------------------------------------
+#include ../../../<NetworkTaskManager/FileDownloadTask.h>
