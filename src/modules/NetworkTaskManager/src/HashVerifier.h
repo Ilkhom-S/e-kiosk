7efe92803560e5db687548f9eb490a4ba383368f
Ilkhom Safarov <ilhomjon.s@gmail.com>
2026-01-26 16:33:57 +0500

    refactor(include): migrate NetworkTaskManager/HashVerifier.h to public API structure

--- src/modules/NetworkTaskManager/src/HashVerifier.h
+++ src/modules/NetworkTaskManager/src/HashVerifier.h
@@ -1,62 +1,9 @@
-/* @file Верификатор данных по алгоритму MD5. */
+/* @file DEPRECATED - See ../../../<NetworkTaskManager/HashVerifier.h> instead.
+
+MIGRATION NOTE: This file kept for backward compatibility only.
+The class definition has been moved to the public header in ../../../<NetworkTaskManager/HashVerifier.h>.
+All NEW code should include ../../../<NetworkTaskManager/HashVerifier.h>.
+*/
 
 #pragma once
-
-#include <Common/QtHeadersBegin.h>
-#include <QtCore/QString>
-#include <QtCore/QByteArray>
-#include <Common/QtHeadersEnd.h>
-
-#include "IVerifier.h"
-
-//------------------------------------------------------------------------
-namespace CHashVerifier {
-    const int MD5HashSize = 32;
-    const int Sha256HashSize = 64;
-} // namespace CHashVerifier
-
-class IHashVerifier : public IVerifier {
-  public:
-    virtual QString referenceHash() const = 0;
-    virtual QString calculatedHash() const = 0;
-};
-
-//------------------------------------------------------------------------
-class Md5Verifier : public IHashVerifier {
-  public:
-    Md5Verifier(const QString &aMD5);
-
-    virtual bool verify(NetworkTask *aTask, const QByteArray &aData);
-
-    QString referenceHash() const {
-        return mMD5;
-    }
-    QString calculatedHash() const {
-        return mCalculatedMD5;
-    }
-
-  private:
-    QString mMD5;
-    QString mCalculatedMD5;
-};
-
-//------------------------------------------------------------------------
-class Sha256Verifier : public IHashVerifier {
-  public:
-    Sha256Verifier(const QString &aSha256);
-
-    virtual bool verify(NetworkTask *aTask, const QByteArray &aData);
-
-    QString referenceHash() const {
-        return mSha256;
-    }
-    QString calculatedHash() const {
-        return mCalculatedSha256;
-    }
-
-  private:
-    QString mSha256;
-    QString mCalculatedSha256;
-};
-
-//------------------------------------------------------------------------
+#include ../../../<NetworkTaskManager/HashVerifier.h>
