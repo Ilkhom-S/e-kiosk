579f114fe1f2698483b077b834ab392062d6a76a
Ilkhom Safarov <ilhomjon.s@gmail.com>
2026-01-29 20:09:23 +0500

    feat(WatchService): add translation files for multi-language support
    - Add English, Russian, Uzbek, and Tajik translation files
    - Translation files: watchservice_en.ts, watchservice_ru.ts, watchservice_uz.ts, watchservice_tg.ts
    - Enables localized UI strings in WatchService application
    - Translations will be compiled to .qm files and loaded at runtime
    
    Completes translation setup for WatchService application.

---
+++ apps/WatchService/src/locale/watchservice_tg.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="tg">
+  <context>
+    <name>SplashScreenClass</name>
+    <message>
+      <location filename="../SplashScreen.ui" line="66" />
+      <source>#terminal_not_available</source>
+      <translation>Терминал муваққатан кор намекунад</translation>
+    </message>
+    <message>
+      <source>#terminal_name</source>
+      <translation type="obsolete">Терминал %1: %2</translation>
+    </message>
+    <message>
+      <source>#support_phone</source>
+      <translation type="obsolete">Телефони дастгирии техникӣ: </translation>
+    </message>
+  </context>
+</TS>
