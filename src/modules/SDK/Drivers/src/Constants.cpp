ad5db60434aa4b9924a6509829bb951458075801
Ilkhom Safarov <ilhomjon.s@gmail.com>
2026-02-08 10:50:01 +0500

    fix(modules/SDK/Drivers): remove redundant extern from constant definitions

--- src/modules/SDK/Drivers/src/Constants.cpp
+++ src/modules/SDK/Drivers/src/Constants.cpp
@@ -48,93 +48,93 @@ int Type6 = qRegisterMetaType<SDK::Driver::ECardType::Enum>();
 namespace CAllHardware {
 
 // Общие константы.
-extern const char RequiredDevice[] = "required_device";
-extern const char DeviceData[] = "device_data";
-extern const char ModelName[] = "model_name";
-extern const char ProtocolName[] = "protocol_name";
-extern const char DetectingPriority[] = "detect_priority";
-extern const char Mask[] = "mask";
-extern const char SystemName[] = "system_name";
-extern const char SearchingType[] = "searching_type";
-extern const char RequiredResource[] = "required_resource";
-extern const char Existence[] = "existence";
-extern const char InteractionType[] = "interaction_type";
-extern const char WaitUpdatingTimeout[] = "wait_updating_timeout";
-extern const char OperatorPresence[] = "operator_presence";
-extern const char FiscalServerPresence[] = "fiscal_server_presence";
-extern const char OptionalPortSettings[] = "optional_port_settings";
-extern const char OptionalPortSettingsEnable[] = "optional_port_settings_enable";
-extern const char SerialNumber[] = "serial_number";
-extern const char CanOnline[] = "can_online";
-extern const char LibraryVersion[] = "library_version";
+const char RequiredDevice[] = "required_device";
+const char DeviceData[] = "device_data";
+const char ModelName[] = "model_name";
+const char ProtocolName[] = "protocol_name";
+const char DetectingPriority[] = "detect_priority";
+const char Mask[] = "mask";
+const char SystemName[] = "system_name";
+const char SearchingType[] = "searching_type";
+const char RequiredResource[] = "required_resource";
+const char Existence[] = "existence";
+const char InteractionType[] = "interaction_type";
+const char WaitUpdatingTimeout[] = "wait_updating_timeout";
+const char OperatorPresence[] = "operator_presence";
+const char FiscalServerPresence[] = "fiscal_server_presence";
+const char OptionalPortSettings[] = "optional_port_settings";
+const char OptionalPortSettingsEnable[] = "optional_port_settings_enable";
+const char SerialNumber[] = "serial_number";
+const char CanOnline[] = "can_online";
+const char LibraryVersion[] = "library_version";
 
 // Значения настроек.
 namespace Values {
-extern const char Use[] = "use";
-extern const char NotUse[] = "not_use";
-extern const char Auto[] = "auto";
+const char Use[] = "use";
+const char NotUse[] = "not_use";
+const char Auto[] = "auto";
 } // namespace Values
 
 // Типы поиска устройств.
 namespace SearchingTypes {
-extern const char Loading[] = "loading";
-extern const char AutoDetecting[] = "auto_detecting";
+const char Loading[] = "loading";
+const char AutoDetecting[] = "auto_detecting";
 } // namespace SearchingTypes
 
 // Варианты нахождения устройств.
 namespace ExistenceTypes {
-extern const char Unique[] = "unique";
-extern const char Multiple[] = "multiple";
+const char Unique[] = "unique";
+const char Multiple[] = "multiple";
 } // namespace ExistenceTypes
 
 // Константы устройств приема денег.
 namespace CashAcceptor {
-extern const char SystemCurrencyId[] = "system_currency_id";
+const char SystemCurrencyId[] = "system_currency_id";
 } // namespace CashAcceptor
 
 // Константы фискального регистратора.
 namespace FR {
-extern const char FSSerialNumber[] = "fs_serial_number";
-extern const char TaxSystems[] = "tax_systems";
-extern const char AgentFlags[] = "agent_flags";
-extern const char AgentFlagsData[] = "agent_flags_data";
-extern const char SectionNames[] = "section_names";
-extern const char DealerTaxSystem[] = "dealer_tax_system";
-extern const char DealerAgentFlag[] = "dealer_agent_flag";
-extern const char DealerVAT[] = "dealer_vat";
-extern const char DealerSupportPhone[] = "dealer_support_phone";
-extern const char UserPhone[] = "user_phone";
-extern const char UserMail[] = "user_mail";
-extern const char ZReportTime[] = "z_report_time";
-extern const char FiscalFieldData[] = "fiscal_field_data";
-extern const char WithoutPrinting[] = "without_printing";
-extern const char CanWithoutPrinting[] = "can_without_printing";
-extern const char NullingSumInCash[] = "nulling_sum_in_cash";
+const char FSSerialNumber[] = "fs_serial_number";
+const char TaxSystems[] = "tax_systems";
+const char AgentFlags[] = "agent_flags";
+const char AgentFlagsData[] = "agent_flags_data";
+const char SectionNames[] = "section_names";
+const char DealerTaxSystem[] = "dealer_tax_system";
+const char DealerAgentFlag[] = "dealer_agent_flag";
+const char DealerVAT[] = "dealer_vat";
+const char DealerSupportPhone[] = "dealer_support_phone";
+const char UserPhone[] = "user_phone";
+const char UserMail[] = "user_mail";
+const char ZReportTime[] = "z_report_time";
+const char FiscalFieldData[] = "fiscal_field_data";
+const char WithoutPrinting[] = "without_printing";
+const char CanWithoutPrinting[] = "can_without_printing";
+const char NullingSumInCash[] = "nulling_sum_in_cash";
 } // namespace FR
 
 // Константы принтера.
 namespace Printer {
-extern const char LineSize[] = "line_size";
-extern const char ReceiptTemplate[] = "receipt_template";
-extern const char PrintingMode[] = "printing_mode";
-extern const char ServiceOperation[] = "service_operation";
-extern const char BlockTerminalOnError[] = "block_terminal_on_error";
-extern const char OFDNotSentError[] = "ofd_not_sent_error";
+const char LineSize[] = "line_size";
+const char ReceiptTemplate[] = "receipt_template";
+const char PrintingMode[] = "printing_mode";
+const char ServiceOperation[] = "service_operation";
+const char BlockTerminalOnError[] = "block_terminal_on_error";
+const char OFDNotSentError[] = "ofd_not_sent_error";
 } // namespace Printer
 
 // Константы HID-устройств.
 namespace HID {
-extern const char Text[] = "text";                         // QString
-extern const char Image[] = "image";                       // QImage
-extern const char FaceDetected[] = "face_detected";        // bool
-extern const char ImageWithFaceArea[] = "image_with_face"; // QImage
+const char Text[] = "text";                         // QString
+const char Image[] = "image";                       // QImage
+const char FaceDetected[] = "face_detected";        // bool
+const char ImageWithFaceArea[] = "image_with_face"; // QImage
 } // namespace HID
 
 // Константы порта.
 namespace Port {
 namespace TCP {
-extern const char IP[] = "ip";
-extern const char Number[] = "port_number";
+const char IP[] = "ip";
+const char Number[] = "port_number";
 } // namespace TCP
 } // namespace Port
 
@@ -142,106 +142,106 @@ extern const char Number[] = "port_number";
 namespace FiscalFields {
 
 // Basic fields
-extern const char FDName[] = "fd_name";
-extern const char UserContact[] = "user_contact";
-extern const char PayOffAddress[] = "payoff_address";
-extern const char FDDateTime[] = "fd_date_time";
-extern const char SerialFRNumber[] = "serial_fr_number";
-extern const char OFDINN[] = "ofd_inn";
-extern const char INN[] = "inn";
-extern const char PayOffAmount[] = "payoff_amount";
-extern const char Cashier[] = "cashier";
-extern const char UnitName[] = "unit_name";
-extern const char AutomaticNumber[] = "automatic_number";
-extern const char RNM[] = "rnm";
-extern const char SessionNumber[] = "session_number";
-extern const char FDNumber[] = "fd_number";
-extern const char SerialFSNumber[] = "serial_fs_number";
-extern const char DocumentNumber[] = "document_number";
-extern const char OFDName[] = "ofd_name";
-extern const char LegalOwner[] = "legal_owner";
-extern const char PayOffType[] = "payoff_type";
-extern const char TaxSystem[] = "tax_system";
-extern const char FTSURL[] = "fts_url";
-extern const char TaxSystemsReg[] = "tax_systems_reg";
-extern const char ProcessingPhone[] = "processing_phone";
-extern const char FDSign[] = "fd_sign";
-extern const char OFDNotSentFDQuantity[] = "ofd_not_sent_fd_quantity";
-extern const char OFDNotSentFDDateTime[] = "ofd_not_sent_fd_date_time";
-extern const char ReregistrationCause[] = "reregistration_cause";
-extern const char FDForSessionTotal[] = "fd_for_session_total";
-extern const char SenderMail[] = "sender_mail";
-extern const char FiscalsForSessionTotal[] = "fiscals_for_session_total";
-extern const char PayOffPlace[] = "payoff_place";
-extern const char ModelVersion[] = "model_version";
-extern const char FFDFR[] = "ffd_fr";
-extern const char FFDFS[] = "ffd_fs";
-extern const char VATRate[] = "vat_rate";
-extern const char CashierINN[] = "cashier_inn";
-extern const char OFDURL[] = "ofd_url";
-extern const char FFD[] = "ffd";
+const char FDName[] = "fd_name";
+const char UserContact[] = "user_contact";
+const char PayOffAddress[] = "payoff_address";
+const char FDDateTime[] = "fd_date_time";
+const char SerialFRNumber[] = "serial_fr_number";
+const char OFDINN[] = "ofd_inn";
+const char INN[] = "inn";
+const char PayOffAmount[] = "payoff_amount";
+const char Cashier[] = "cashier";
+const char UnitName[] = "unit_name";
+const char AutomaticNumber[] = "automatic_number";
+const char RNM[] = "rnm";
+const char SessionNumber[] = "session_number";
+const char FDNumber[] = "fd_number";
+const char SerialFSNumber[] = "serial_fs_number";
+const char DocumentNumber[] = "document_number";
+const char OFDName[] = "ofd_name";
+const char LegalOwner[] = "legal_owner";
+const char PayOffType[] = "payoff_type";
+const char TaxSystem[] = "tax_system";
+const char FTSURL[] = "fts_url";
+const char TaxSystemsReg[] = "tax_systems_reg";
+const char ProcessingPhone[] = "processing_phone";
+const char FDSign[] = "fd_sign";
+const char OFDNotSentFDQuantity[] = "ofd_not_sent_fd_quantity";
+const char OFDNotSentFDDateTime[] = "ofd_not_sent_fd_date_time";
+const char ReregistrationCause[] = "reregistration_cause";
+const char FDForSessionTotal[] = "fd_for_session_total";
+const char SenderMail[] = "sender_mail";
+const char FiscalsForSessionTotal[] = "fiscals_for_session_total";
+const char PayOffPlace[] = "payoff_place";
+const char ModelVersion[] = "model_version";
+const char FFDFR[] = "ffd_fr";
+const char FFDFS[] = "ffd_fs";
+const char VATRate[] = "vat_rate";
+const char CashierINN[] = "cashier_inn";
+const char OFDURL[] = "ofd_url";
+const char FFD[] = "ffd";
 
 // Transfer operator data
-extern const char TransferOperatorAddress[] = "transfer_operator_address";
-extern const char TransferOperatorINN[] = "transfer_operator_inn";
-extern const char TransferOperatorName[] = "transfer_operator_name";
-extern const char TransferOperatorPhone[] = "transfer_operator_phone";
+const char TransferOperatorAddress[] = "transfer_operator_address";
+const char TransferOperatorINN[] = "transfer_operator_inn";
+const char TransferOperatorName[] = "transfer_operator_name";
+const char TransferOperatorPhone[] = "transfer_operator_phone";
 
 // Provider data
-extern const char ProviderPhone[] = "provider_phone";
-extern const char ProviderINN[] = "provider_inn";
+const char ProviderPhone[] = "provider_phone";
+const char ProviderINN[] = "provider_inn";
 
 // Payment agent data
-extern const char AgentOperation[] = "agent_operation";
-extern const char AgentFlagsReg[] = "agent_flags_reg";
-extern const char AgentPhone[] = "agent_phone";
-extern const char AgentFlag[] = "agent_flag";
+const char AgentOperation[] = "agent_operation";
+const char AgentFlagsReg[] = "agent_flags_reg";
+const char AgentPhone[] = "agent_phone";
+const char AgentFlag[] = "agent_flag";
 
 // Statuses
-extern const char FSExpiredStatus[] = "fs_expired_status";
-extern const char FSNeedChangeStatus[] = "fs_need_change_status";
-extern const char FSMemoryEnd[] = "fs_memory_end_status";
-extern const char OFDNoConnection[] = "ofd_no_connection_status";
+const char FSExpiredStatus[] = "fs_expired_status";
+const char FSNeedChangeStatus[] = "fs_need_change_status";
+const char FSMemoryEnd[] = "fs_memory_end_status";
+const char OFDNoConnection[] = "ofd_no_connection_status";
 
 // Operating modes
-extern const char AutomaticMode[] = "automatic_mode";
-extern const char AutonomousMode[] = "autonomous_mode";
-extern const char EncryptionMode[] = "encryption_mode";
-extern const char InternetMode[] = "internet_mode";
-extern const char ServiceAreaMode[] = "service_area_mode";
-extern const char FixedReportingMode[] = "fixed_reporting_mode";
-extern const char LotteryMode[] = "lottery_mode";
-extern const char GamblingMode[] = "gambling_mode";
-extern const char ExcisableUnitMode[] = "excisable_unit_mode";
-extern const char InAutomateMode[] = "in_automate_mode";
+const char AutomaticMode[] = "automatic_mode";
+const char AutonomousMode[] = "autonomous_mode";
+const char EncryptionMode[] = "encryption_mode";
+const char InternetMode[] = "internet_mode";
+const char ServiceAreaMode[] = "service_area_mode";
+const char FixedReportingMode[] = "fixed_reporting_mode";
+const char LotteryMode[] = "lottery_mode";
+const char GamblingMode[] = "gambling_mode";
+const char ExcisableUnitMode[] = "excisable_unit_mode";
+const char InAutomateMode[] = "in_automate_mode";
 
 // Payoff subject (per item)
-extern const char PayOffSubjectQuantity[] = "payoff_subject_quantity";
-extern const char PayOffSubjectAmount[] = "payoff_subject_amount";
-extern const char PayOffSubject[] = "payoff_subject";
-extern const char PayOffSubjectUnitPrice[] = "payoff_subject_unit_price";
-extern const char PayOffSubjectTaxAmount[] = "payoff_subject_tax_amount";
-extern const char PayOffSubjectType[] = "payoff_subject_type";
-extern const char PayOffSubjectMethodType[] = "payoff_subject_method_type";
+const char PayOffSubjectQuantity[] = "payoff_subject_quantity";
+const char PayOffSubjectAmount[] = "payoff_subject_amount";
+const char PayOffSubject[] = "payoff_subject";
+const char PayOffSubjectUnitPrice[] = "payoff_subject_unit_price";
+const char PayOffSubjectTaxAmount[] = "payoff_subject_tax_amount";
+const char PayOffSubjectType[] = "payoff_subject_type";
+const char PayOffSubjectMethodType[] = "payoff_subject_method_type";
 
 // Fiscal totals by payment method (per receipt)
-extern const char CashFiscalTotal[] = "cash_fiscal_total";
-extern const char CardFiscalTotal[] = "card_fiscal_total";
-extern const char PrePaymentFiscalTotal[] = "prepayment_fiscal_total";
-extern const char PostPaymentFiscalTotal[] = "postpayment_fiscal_total";
-extern const char CounterOfferFiscalTotal[] = "counter_offer_fiscal_total";
+const char CashFiscalTotal[] = "cash_fiscal_total";
+const char CardFiscalTotal[] = "card_fiscal_total";
+const char PrePaymentFiscalTotal[] = "prepayment_fiscal_total";
+const char PostPaymentFiscalTotal[] = "postpayment_fiscal_total";
+const char CounterOfferFiscalTotal[] = "counter_offer_fiscal_total";
 
 // Taxes (per receipt)
-extern const char TaxAmount02[] = "tax_amount_02";
-extern const char TaxAmount03[] = "tax_amount_03";
-extern const char TaxAmount04[] = "tax_amount_04";
-extern const char TaxAmount05[] = "tax_amount_05";
-extern const char TaxAmount06[] = "tax_amount_06";
-extern const char TaxAmount07[] = "tax_amount_07";
+const char TaxAmount02[] = "tax_amount_02";
+const char TaxAmount03[] = "tax_amount_03";
+const char TaxAmount04[] = "tax_amount_04";
+const char TaxAmount05[] = "tax_amount_05";
+const char TaxAmount06[] = "tax_amount_06";
+const char TaxAmount07[] = "tax_amount_07";
 
 // Values
 namespace Values {
-extern const char NoData[] = "none";
+const char NoData[] = "none";
 } // namespace Values
 
 } // namespace FiscalFields
@@ -249,37 +249,37 @@ extern const char NoData[] = "none";
 } // namespace CAllHardware
 
 // Application component
-extern const char Application[] = "Common";
+const char Application[] = "Common";
 
 // Components definitions
 namespace CComponents {
-extern const char Driver[] = "Driver";
-extern const char IOPort[] = "IOPort";
-extern const char BillAcceptor[] = "BillAcceptor";
-extern const char CoinAcceptor[] = "CoinAcceptor";
-extern const char Dispenser[] = "Dispenser";
-extern const char Printer[] = "Printer";
-extern const char FiscalRegistrator[] = "FiscalRegistrator";
-extern const char DocumentPrinter[] = "DocumentPrinter";
-extern const char Watchdog[] = "Watchdog";
-extern const char Modem[] = "Modem";
-extern const char Scanner[] = "Scanner";
-extern const char CardReader[] = "CardReader";
-extern const char Health[] = "Health";
-extern const char Camera[] = "Camera";
-extern const char Token[] = "Token";
+const char Driver[] = "Driver";
+const char IOPort[] = "IOPort";
+const char BillAcceptor[] = "BillAcceptor";
+const char CoinAcceptor[] = "CoinAcceptor";
+const char Dispenser[] = "Dispenser";
+const char Printer[] = "Printer";
+const char FiscalRegistrator[] = "FiscalRegistrator";
+const char DocumentPrinter[] = "DocumentPrinter";
+const char Watchdog[] = "Watchdog";
+const char Modem[] = "Modem";
+const char Scanner[] = "Scanner";
+const char CardReader[] = "CardReader";
+const char Health[] = "Health";
+const char Camera[] = "Camera";
+const char Token[] = "Token";
 } // namespace CComponents
 
 //---------------------------------------------------------------------------
 // Definitions for CInteractionTypes constants
 namespace CInteractionTypes {
-extern const char COM[] = "COM";
-extern const char USB[] = "USB";
-extern const char LibUSB[] = "LibUSB";
-extern const char TCP[] = "TCP";
-extern const char OPOS[] = "OPOS";
-extern const char System[] = "System";
-extern const char External[] = "External";
+const char COM[] = "COM";
+const char USB[] = "USB";
+const char LibUSB[] = "LibUSB";
+const char TCP[] = "TCP";
+const char OPOS[] = "OPOS";
+const char System[] = "System";
+const char External[] = "External";
 } // namespace CInteractionTypes
 
 } // namespace Driver
